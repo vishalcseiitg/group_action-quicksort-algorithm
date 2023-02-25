f78730b735f4880eab5f8f5f0b52f0dff20696ad
Vishal Kumar <vishalsharma.viz@gmail.com>
2023-02-26 03:33:32 +0530

    Update eligible_pivot.py

--- eligible_pivot.py
+++ eligible_pivot.py
@@ -1,4 +1,9 @@
-def eligible_pivot(arr):
+def eligible_pivot(input_file, output_file):
+    # Open the input file in read mode
+    with open(input_file, 'r') as file:
+        # Read the contents of the file into a list of integers
+        arr = [int(line.strip()) for line in file]
+
     n = len(arr)
     eligible = []
     for i in range(n):
@@ -9,4 +14,11 @@ def eligible_pivot(arr):
                 break
         if is_eligible:
             eligible.append(arr[i])
-    return eligible
+
+    # Open the output file in write mode
+    with open(output_file, 'w') as file:
+        # Write the eligible pivot elements to the output file
+        file.write('\n'.join(map(str, eligible)))
+
+# Call the function with input and output file paths
+eligible_pivot('input.txt', 'output.txt')
